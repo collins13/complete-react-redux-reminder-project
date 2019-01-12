a216fd616d8eb247c96358c988c8770fa5fcaf18
frank collins <frank@localhost.localdomain>
2019-01-12 16:24:55 -0500

    Updates

---
+++ precache-manifest.2214f31d7210834e3aedfc9eac2bf042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c87d8fc0d456902970f9",
+    "url": "/complete-react-redux-reminder-project/static/css/main.51fdde40.chunk.css"
+  },
+  {
+    "revision": "c87d8fc0d456902970f9",
+    "url": "/complete-react-redux-reminder-project/static/js/main.c87d8fc0.chunk.js"
+  },
+  {
+    "revision": "cf978f44624ed8ba5de4",
+    "url": "/complete-react-redux-reminder-project/static/js/1.cf978f44.chunk.js"
+  },
+  {
+    "revision": "9560665b1c95db7ecaa7",
+    "url": "/complete-react-redux-reminder-project/static/js/runtime~main.9560665b.js"
+  },
+  {
+    "revision": "dc1630d498dd164cb46cd79645888798",
+    "url": "/complete-react-redux-reminder-project/index.html"
+  }
+];
